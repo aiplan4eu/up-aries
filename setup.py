c65d5c02b7c86620e9a11ae653229f2ab76d7d41
franklinselva <franklinselva10@gmail.com>
2022-07-19 12:24:57 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -2,18 +2,20 @@
 
 from setuptools import setup
 
-long_description = """============================================================
-    UP - ARIES
- ============================================================
-"""
+long_description = ""
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name="up_aries",
-    version="0.0.1",
-    description="Aries is a project aimed at exploring constraint-based techniques for automated planning and scheduling. It relies on an original implementation of constraint solver with optional variables and clause learning to which various automated planning problems can be submitted.",
+    version="0.0.2",
+    description="Aries is a project aimed at exploring constraint-based techniques for automated planning and scheduling. \
+        It relies on an original implementation of constraint solver with optional variables and clause learning to which \
+        various automated planning problems can be submitted.",
     author="CNRS-LAAS",
     author_email="abitmonnot@laas.fr",
     packages=["up_aries"],
-    url="https://github.com/plaans/aries", # TODO: Add documentation URL if any
+    url="https://github.com/plaans/aries",  # TODO: Add documentation URL if any
     license="MIT",
 )
