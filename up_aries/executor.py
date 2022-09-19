494f996959dd04920bdc4285dad30bbd7757dd28
franklin selva <franklinselva10@gmail.com>
2022-09-19 12:00:15 +0200

    replace same binary for macos

--- up_aries/executor.py
+++ up_aries/executor.py
@@ -5,7 +5,7 @@ import platform
 EXECUTABLES = {
     ("Linux", "x86_64"): "bins/aries_linux_x86_64",
     ("Linux", "aarch64"): "bins/aries_linux_aarch64",
-    ("Darwin", "x86_64"): "bins/aries_macos_x86_64",
+    ("Darwin", "x86_64"): "bins/aries_macos_aarch64",
     ("Darwin", "aarch64"): "bins/aries_macos_aarch64",
     # ("Windows", "x86_64"): "aries_windows_x86_64.exe",
     # ("Windows", "aarch64"): "aries_windows_aarch64.exe",
